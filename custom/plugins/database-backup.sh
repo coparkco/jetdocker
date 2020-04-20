73fb9a822622a80c094d81e02f78e705d53e6ff9
nohammekharbeche <nm@copark.co>
2020-04-20 12:58:54 +0200

    Test

--- custom/plugins/database-backup.sh
+++ custom/plugins/database-backup.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-
+# path /var/mysql_backup/cameleon/
+# serverName h130@185.116.106.104
+# file pattern
 DatabaseBackup::Fetch()
 {
     namespace database-backup
     ${DEBUG} && Log::AddOutput database-backup DEBUG
     Log "DatabaseBackup::Fetch"
 
-    pwd
+    identity_file="../.ssh/preprod_rsa"
+    dir="/var/mysql_backup/cameleon/"
+    server="h130@185.116.106.104"
+    scp -i $identity_file $server:$dir/$(ssh $server 'ls -t $dir | head -1') .
 }
